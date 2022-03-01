59e1528ba4e18014b5ae654f07e51fbfc0adea3d
Mrhaltung <dipeshchandekar99@gmail.com>
2022-03-01 13:21:35 +0530

    [Mrhaltung] :: Register, Login, Forget, Reset completed

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -30,7 +30,6 @@ namespace BookStore
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddRazorPages();
@@ -96,7 +95,6 @@ namespace BookStore
             }));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -106,7 +104,6 @@ namespace BookStore
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
